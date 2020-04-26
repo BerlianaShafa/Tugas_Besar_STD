c2f1eb0b1daa94961be06e6860eb19b8ed54071e
BerlianaShafa <59946995+BerlianaShafa@users.noreply.github.com>
2020-04-26 22:49:59 +0700

    Update header list parent dan child  from Berliana

---
+++ listChild.h
@@ -0,0 +1,50 @@
+#ifndef LISTCHILD_H_INCLUDED
+#define LISTCHILD_H_INCLUDED
+#include <iostream>
+#define nextChild(P) P->nextChild
+#define prevChild(P) P->prevChild
+#define infoChild(P) P->infoChild
+#define firstChild(L) L.firstChild
+#define busana(P) P->busana
+
+// Double Circular
+// child : busana
+using namespace std;
+
+struct infotypeChild
+{
+    int kodeBusana;
+    string jenisBusana;
+    string ukuranBusana;
+    int jumlahBusana;
+};
+
+typedef struct elmlistChild *addressChild;
+
+struct elmlistChild
+{
+    infotypeChild infoChild;
+    addressChild nextChild;
+    addressChild prevChild;
+};
+
+struct listChild
+{
+    addressChild firstChild;
+};
+
+void createListChild(listChild &L);
+addressChild allocateChild(int code, string kindof, string ukuran, int jumlah);
+void insertFirstChild(listChild &L, addressChild P);
+void insertAfterChild(listChild &L, addressChild prec, addressChild P);
+void insertLastChild(listChild &L, addressChild P);
+void insertSortChild(listChild &L, addressChild P);
+void deleteFirstChild(listChild &L, addressChild &P);
+void deleteAfterChild(listChild &L, addressChild prec, addressChild &P);
+void deleteLastChild(listChild &L, addressChild &P);
+void printinfoChild(listChild L);
+addressChild searchElmChild(listChild &L, int kode);
+
+
+
+#endif // LISTCHILD_H_INCLUDED
---
+++ listParent.h
@@ -0,0 +1,49 @@
+#ifndef LISTPARENT_H_INCLUDED
+#define LISTPARENT_H_INCLUDED
+#define <iostream>
+#define nextParent(P) P->nextParent
+#define infoParent(P) P->infoParent
+#define firstParent(L) L.firstParent
+#define peminjam(P) P->peminjam
+
+//Single linked list
+//parent : penyewa
+
+using namespace std;
+
+struct infotypeParent
+{
+    int idPenyewa;
+    string alamatPenyewa;
+    string namaPenyewa;
+    int umurPenyewa;
+};
+
+typedef struct elmlistParent *addressParent;
+
+struct elmlistParent
+{
+    infotypeParent infoParent;
+    addressParent nextParent;
+};
+
+struct listParent
+{
+    addressParent firstParent;
+};
+
+void createListParent(listParent &L);
+addressParent allocateParent(int id, string alamat, string name, int ages);
+void insertFirstParent(listParent &L, addressParent P);
+void insertAfterParent(listParent &L, addressParent prec, addressParent P);
+void insertLastParent(listParent &L, addressParent P);
+void insertSortParent(listParent &L, addressParent P);
+void deleteFirstParent(listParent &L, addressParent &P);
+void deleteAfterParent(listParent &L, addressParent prec, addressParent &P);
+void deleteLastParent(listParent &L, addressParent &P);
+void printinfoParent(listParent L);
+addressParent searchElmParent(listParent &L, int ID);
+
+
+
+#endif // LISTPARENT_H_INCLUDED
